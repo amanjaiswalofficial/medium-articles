d09a28a68d3e94d33941bab8e0c9e77ca87a81e6
amanjaiswalofficial <amanjai01@gmail.com>
2023-12-08 21:15:53 +0530

    Added code for validation

--- validate.py
+++ validate.py
@@ -1,3 +1,7 @@
 from main import GreatExpectationsManager
 suite_name = "my_suite_v1"
 input_data_path = "data.csv"
+
+ge_manager = GreatExpectationsManager(suite_name)
+validation_result = ge_manager.validate(input_data_path)
+print(validation_result)
