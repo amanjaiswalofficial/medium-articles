0b41a30e734cc8716ee2989ca79ad74f1b90f5a2
amanjaiswalofficial <amanjai01@gmail.com>
2023-12-18 21:30:45 +0530

    Moved code for validate.py

--- great_expectations_demo/generate.py
+++ great_expectations_demo/generate.py
@@ -8,3 +8,4 @@ expectations_list = config_object.get_expectations_list()
 ge_manager = GreatExpectationsManager('my_suite_v1')
 ge_manager.generate_suite(expectations_list)
 ge_manager.generate_and_open_data_docs()
+
